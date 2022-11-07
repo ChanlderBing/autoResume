6b3d66e8f5bc4cf29c9eb4f2c2766835c480774d
joe <983270722@qq.com>
2022-11-07 20:03:03 +0800

    头像

--- src/components/Resume/components/Personal.vue
+++ src/components/Resume/components/Personal.vue
@@ -8,11 +8,11 @@
     <!-- <div class="edu"></div>
     <div class="email">983270722@qq.com</div> -->
   </div>
-
   
-  <div class="avatar">
-    <img :src="imgSrc" alt="Ikun!" blue @click="upload">
-      </div> 
+  <div class="avatar" >
+    <img :src=imgSrc alt="Ikun!" blue @click="upload" class="aa"  @mouseleave="isShow = false;"  @mouseenter="isShow = true">
+    <div class="mask" v-show="isShow"></div>
+    </div> 
       <input type="file" name="logo" style="display: none" ref="inputFile" @change="updateFile">
       <input type="submit" value="提交" hidden>
   </div>
@@ -22,20 +22,20 @@
 import { onMounted, ref } from "vue";
 
 const inputFile = ref(null)
-const imgSrc  =  ref("@/assets/img/wyk.jpg")
-  const upload = ()=>{
-   const a = inputFile.value.click()
-    console.log(a);
-  }
-  const updateFile =(e)=>{
-    const file = e.target.files[0]
-    let img = new FileReader()
-    img.readAsDataURL(e.target.files[0])
-    img.onload = ({target})=>{
-      imgSrc.value = target.result as string
-    }
+const imgSrc  =  ref(require('@/assets/img/wyk.jpg'))
+const isShow = ref(false)
 
+const upload = ()=>{
+   const a = inputFile.value.click()
+}
+const updateFile =(e)=>{
+  const file = e.target.files[0]
+  let img = new FileReader()
+  img.readAsDataURL(e.target.files[0])
+  img.onload = ({target})=>{
+    imgSrc.value = target.result as string
   }
+}
 
 </script>
 
@@ -52,13 +52,39 @@ const imgSrc  =  ref("@/assets/img/wyk.jpg")
   }
   .avatar{
     width: 40%;
+    position: relative;
     img{
       padding-left: 180px;
       height: 100px;
-      width: 80px;
+      width: 100px;
+      margin: 15px;
+      //-webkit-mask: linear-gradient(90deg, transparent, #fff);
+    }
+    .mask{
+      height: 100px;
+      width: 100px;
+      position: absolute;
+      right: 16px;
+      bottom: 17px;
+      background: rgba(64,64,64,.5);
+      text-align: right;
+      cursor: auto; 
     }
   }
 }
 
 
+// .avatar::after {
+//     content: "";
+//     position: absolute;
+//     top: 0;
+//     left: 0;
+//     bottom: 0;
+//     right: 0;
+//     height: 100px;
+//     width: 80px;
+//     background: #000 no-repeat;
+//     background-size: cover;
+//     mask: linear-gradient(110deg, #000 10%, transparent 70%, transparent);
+// }
 </style>
